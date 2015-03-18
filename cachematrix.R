950943b677316130b14a489be26ec3442bfd45a1
Michelle Muha <michelle.muha@gmail.com>
2015-03-17 22:39:09 -0700

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script consists of 2 functions which can be used to invert a matrix,
+## store the inverted matrix in cache and recall it for later use
 
-## Write a short comment describing this function
+## This function computes the inverse of a matrix, x, and caches it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  set <- function(y){
+    z <- solve(x)
+    z <<- y
+  }
+  get <- function() z
 }
 
-
-## Write a short comment describing this function
+## This function checks to see if the inverse of the matrix has been stored in cache
+## If the inverted matrix is in the cache, the function returns it
+## If no matrix is stored in the cache, 
+## the function computes the inverse of the matrix and returns
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- get ()
+    if(!is.null(m)){
+          message("getting cached data")
+          return(m)
+    }
+    z <- solve (x)
+    z
+    
 }
